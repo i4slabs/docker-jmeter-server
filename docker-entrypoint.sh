19c9c87c2b185daf4f4df81d98c272d3df06506d
adriancm <adrian.cepillo@i4s.com>
2017-07-10 18:14:40 +0200

    Resolves another bug in jmeter-server script execution

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -7,7 +7,7 @@ n=$(($freeMem/10*2))
 export JVM_ARGS="-Xmn${n}m -Xms${s}m -Xmx${x}m"
 JMETER_LOG="jmeter-server.log" && touch $JMETER_LOG && tail -f $JMETER_LOG &
 exec jmeter-server \
-    -Dserver_port=${SERVER_PORT}
+    -Dserver_port=${SERVER_PORT} \
     -Djava.rmi.server.hostname=${IP} \
     -Dserver.rmi.port=${SERVER_PORT} \
     -Dserver.rmi.localport=${RMI_LOCALPORT} \
