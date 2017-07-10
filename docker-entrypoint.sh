903bbd3793381d5704f0e068d73beaa5e59d4b13
adriancm <adrian.cepillo@i4s.com>
2017-07-10 17:46:13 +0200

    Adds again java.rmi.server.hostname var

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -8,7 +8,7 @@ export JVM_ARGS="-Xmn${n}m -Xms${s}m -Xmx${x}m"
 JMETER_LOG="jmeter-server.log" && touch $JMETER_LOG && tail -f $JMETER_LOG &
 exec jmeter-server \
     -D "server_port=${SERVER_PORT}"
-    #-D "java.rmi.server.hostname=${IP}" \
+    -D "java.rmi.server.hostname=${IP}" \
     -D "server.rmi.port=${SERVER_PORT}" \
     -D "server.rmi.localport=${RMI_LOCALPORT}" \
     -D "server.rmi.localhostname=${LOCALHOSTNAME}"
