33a63b0b445e788033392757b1ca4bd2282c9263
adriancm <adrian.cepillo@i4s.com>
2017-07-10 17:58:19 +0200

    Resolves bug on jmeter server configuration for server hostname

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -7,8 +7,8 @@ n=$(($freeMem/10*2))
 export JVM_ARGS="-Xmn${n}m -Xms${s}m -Xmx${x}m"
 JMETER_LOG="jmeter-server.log" && touch $JMETER_LOG && tail -f $JMETER_LOG &
 exec jmeter-server \
-    -D "server_port=${SERVER_PORT}"
-    -D "java.rmi.server.hostname=${IP}" \
-    -D "server.rmi.port=${SERVER_PORT}" \
-    -D "server.rmi.localport=${RMI_LOCALPORT}" \
-    -D "server.rmi.localhostname=${LOCALHOSTNAME}"
+    -Dserver_port=${SERVER_PORT}
+    -Djava.rmi.server.hostname=${IP} \
+    -Dserver.rmi.port=${SERVER_PORT} \
+    -Dserver.rmi.localport=${RMI_LOCALPORT} \
+    -Dserver.rmi.localhostname=${LOCALHOSTNAME}
